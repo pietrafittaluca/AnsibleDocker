7be71bac981ebe392a43b47199b953424a9f205a
Luca <43339283+pietrafittaluca@users.noreply.github.com>
2020-09-07 11:41:05 +0200

    Create installa_docker_CentOS.sh

---
+++ installa_docker_CentOS.sh
@@ -0,0 +1,23 @@
+# Rimozione di vecchie versioni di Docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+# Installa Docker utilizzando il metodo repository
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+# Start Docker
+sudo systemctl start docker
+
+# Verifica se Docker è installato correttamente
+sudo docker run hello-world
